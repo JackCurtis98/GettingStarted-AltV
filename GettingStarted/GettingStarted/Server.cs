be34e80fa21d0bf3a303eadba5b273435bd94075
JCurtis <jack_curtis98@hotmail.com>
2019-06-09 14:30:56 +1000

    Initial Commit

---
+++ GettingStarted/GettingStarted/Server.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using AltV.Net;
+using AltV.Net.Elements.Entities;
+using AltV.Net.Data;
+using AltV.Net.Enums;
+
+namespace GettingStarted
+{
+    public class Server : Resource
+    {
+        public override void OnStart()
+        {
+            Alt.Log("Server is listening!");
+
+            //Events
+            Alt.OnPlayerConnect += OnPlayerConnect;
+            Alt.OnPlayerDead += OnPlayerDead;
+        }
+
+        private void OnPlayerDead(IPlayer player, IEntity killer, uint weapon)
+        {
+            player.Position = new Position((float)-1070.906250, (float)-2972.122803, (float)13.773568); // LS AIR Position
+            player.GiveWeapon(Weapons.AssaultRifle, 9000, true);
+        }
+
+        private void OnPlayerConnect(IPlayer player, string reason)
+        {
+            Alt.Log($"Player has connected! {player.Name}");
+
+            player.Model = 0x3D843282;
+            player.Position = new Position((float)-1070.906250, (float)-2972.122803, (float)13.773568); // LS AIR Position
+            player.GiveWeapon(Weapons.AssaultRifle, 9000, true);
+        }
+
+        public override void OnStop()
+        {
+            Alt.Log("Server Stopped.");
+        }
+    }
+}
